cbc9e5be88978739608dbc5c2e504432eec571d9
enforest <enforest@gmail.com>
2021-08-26 13:43:52 +0800

    Add files via upload

---
+++ mvwChina-AutoAudit.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         mvwChina-AutoAudit
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://ekmz.mvwchina.com/journal/audit
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(function() {
+    var i;
+    for(i=1;i<document.getElementsByClassName("layui-btn layui-btn-mini layui-btn-radius").length;i+=3) {
+        document.getElementsByClassName("layui-btn layui-btn-mini layui-btn-radius")[i].click();}
+    }, 5000)
+})();
